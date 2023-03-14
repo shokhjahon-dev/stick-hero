7608cd6d9819fd9f2ee90fce025347a68c9d753d
Shokhjahon <112770922+shokhjahon-dev@users.noreply.github.com>
2023-03-14 09:53:45 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-/*
-
-
-
-If you want to know how this game was made, check out this video, that explains how it's made: 
-
-https://youtu.be/eue3UdFvwPo
-
-Follow me on twitter for more: https://twitter.com/HunorBorbely
-
-*/
-
 // Extend the base functionality of JavaScript
 Array.prototype.last = function () {
   return this[this.length - 1];
